bb5aa976fa41b4be6433239c4084e8d96aa32818
PULUGURA  MANJUNATH REDDY <manjunathreddym677@gmail.com>
2024-12-24 00:46:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -346,7 +346,7 @@ app.post('/signup', async (req, res) => {
   }
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(5000, '0.0.0.0', () => {
+    console.log('Server running on http://0.0.0.0:5000');
 });
+
